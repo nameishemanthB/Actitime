7294a3789a912579bac3ba40212b45028248921f
SkillRary <SkillRary@Tyss-SR-021>
2020-06-13 20:03:24 +0530

    Added New Test Case TC004

---
+++ ActiTime/src/tests/TC004.java
@@ -0,0 +1,20 @@
+package tests;
+import org.testng.annotations.Test;
+import genericlibs.ExcelLibrary;
+import pages.HomePage;
+import pages.LoginPage;
+public class TC004 extends BaseTest
+{
+	@Test
+	public void HomePageTest()
+	{
+		LoginPage l = new LoginPage(driver);
+		String un=ExcelLibrary.getValue(XL_PATH, "TC001", 1, 0);
+		String pwd=ExcelLibrary.getValue(XL_PATH, "TC001", 1, 1);
+		l.setUsername(un);
+		l.setPassword(pwd);
+		l.clickLogin();	
+		HomePage h = new HomePage(driver);
+		h.clickRadio();
+	}
+}
