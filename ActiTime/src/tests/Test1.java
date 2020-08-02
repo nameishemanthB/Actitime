ac1989d02b797a5a7d8df477bc29c6980911c245
uday <uday@USER>
2020-08-02 20:23:51 +0530

    added TestCase1,TestCase2,TestCase3,TestCase4

---
+++ ActiTime/src/tests/Test1.java
@@ -0,0 +1,22 @@
+package tests;
+import org.testng.annotations.Test;
+import genericlibs.ExcelLibrary;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class Test1 extends BaseTest
+{
+	@Test
+	public void VerifyHomePageTitle()
+	{
+		LoginPage l = new LoginPage(driver);
+		String un=ExcelLibrary.getValue(XL_PATH, "TestCase1", 1, 0);
+		String pwd=ExcelLibrary.getValue(XL_PATH, "TestCase1", 1, 1);
+		l.setUsername(un);
+		l.setPassword(pwd);
+		l.clickLogin();
+		
+		HomePage h = new HomePage(driver);
+		h.verifyTitle();
+	}
+}
